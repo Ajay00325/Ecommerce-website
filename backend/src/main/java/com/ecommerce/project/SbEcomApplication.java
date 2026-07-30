1c7d40124ed066754ddd9c37991a2e8aed35501a
Ajay00325 <ajay.maddila1337@gmail.com>
2026-07-30 13:41:48 +0530

    Debug image path

--- backend/src/main/java/com/ecommerce/project/SbEcomApplication.java
+++ backend/src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -1,13 +1,22 @@
 package com.ecommerce.project;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbEcomApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbEcomApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        File folder = new File("images");
+
+        System.out.println("=================================");
+        System.out.println("IMAGE PATH = " + folder.getAbsolutePath());
+        System.out.println("IMAGE FOLDER EXISTS = " + folder.exists());
+        System.out.println("=================================");
+
+        SpringApplication.run(SbEcomApplication.class, args);
+    }
 }
