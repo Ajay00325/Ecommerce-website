8622ad44e982febdae798c711a8249506f909186
Ajay00325 <ajay.maddila1337@gmail.com>
2026-07-30 13:36:38 +0530

    Debug image path

--- backend/src/main/java/com/ecommerce/project/config/WebMvcConfig.java
+++ backend/src/main/java/com/ecommerce/project/config/WebMvcConfig.java
@@ -1,13 +1,23 @@
 package com.ecommerce.project.config;
 
+import java.io.File;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+
+        File folder = new File("images");
+
+        System.out.println("IMAGE PATH = " + folder.getAbsolutePath());
+        System.out.println("EXISTS = " + folder.exists());
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:images/");
     }
 }
