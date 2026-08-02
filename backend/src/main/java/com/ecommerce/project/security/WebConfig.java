a60a16dcffbbb53fc7084473a47913aebd637263
Ajay00325 <ajay.maddila1337@gmail.com>
2026-08-02 18:20:24 +0530

    solving the payment page

--- backend/src/main/java/com/ecommerce/project/security/WebConfig.java
+++ backend/src/main/java/com/ecommerce/project/security/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://ecommerce-website-snowy-eight.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
